785844977c49cc5318d31c0db8c2914b05878abc
adrijan78 <adrijan728@gmail.com>
2026-09-20 12:09:35 +0200

    initial commit

---
+++ src/main/java/com/adrijanProjects/todos/TodosApplication.java
@@ -0,0 +1,14 @@
+package com.adrijanProjects.todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TodosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrijanProjects/todos/TodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrijanProjects.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
