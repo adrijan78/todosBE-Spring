0cd34a9575de126d51388668eb642ed4a27fd19d
adrijan78 <adrijan728@gmail.com>
2026-09-21 20:17:10 +0200

    adding jwt auth logic part 1

--- src/main/java/com/adrijanProjects/todos/TodosApplication.java
+++ src/main/java/com/adrijanProjects/todos/TodosApplication.java
@@ -1,10 +1,12 @@
 package com.adrijanProjects.todos;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodosApplication {
+
 
 	public static void main(String[] args) {
 
---
+++ src/main/java/com/adrijanProjects/todos/service/JwtService.java
@@ -0,0 +1,12 @@
+package com.adrijanProjects.todos.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Map;
+
+public interface JwtService {
+
+    String extractUsername(String token);
+    boolean isTokenValid(String token, UserDetails userDetails);
+    String generateToken(Map<String, Object> claims, UserDetails userDetails);
+}
